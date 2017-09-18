3d1aa3b193276eea0269f77726f38256badc63ad
Jared Grippe <jared@deadlyicon.com>
2017-09-17 17:14:50 -0700

    WIP

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const abrade = require('abrade')
 
-abrade.plugin('imdb', {
-  url: 'http://www.imdb.com/find?q=$1?s=tt',
-  entity: {
-    selector: '.findResult',
-    properties: {
-      title: {
-        selector: '.result_text',
-        property: 'innerText',
-      },
-      url: {
-        selector: 'a[href]',
-        property: 'href',
-      }
-    }
+module.exports = abrade.plugin('imdb', {
+  url: 'http://www.imdb.com/find?q=$1&s=tt',
+  entitySelector: '.findResult',
+  properties: {
+    title: node => extractTitle(node).trim(),
+    year: node => extractYear(node),
+    url: node => extractUrl(node),
   }
 })
 
-// abrade.plugin('imdb', query => {
-//   const url = abrade.url('http://www.imdb.com/find',{q: query, s:'tt'})
-//   return abrade.request(url).then(response => {
-//     const $ = cheerio(response)
-//   })
+const extractTitle = node => {
+  let title = node.find('.result_text').text()
+  const matches = title.match(/\s*(.+)\s*\((\d\d\d\d)\)/)
+  if (matches) title = matches[1]
+  return title.slice(0, 100)
+}
 
-// })
+const extractYear = node => {
+  const title = node.find('.result_text').text()
+  const matches = title.match(/\((\d\d\d\d)\)/)
+  if (matches) return Number(matches[1])
+}
+
+const extractUrl = node => {
+  const href = node.find('a[href]').attr('href')
+  return href && href[0] === '/'
+    ? `http://www.imdb.com${href}`
+    : href
+}
